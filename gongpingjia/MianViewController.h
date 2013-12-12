a455ea0df356193192de96be702052c11135cdb6
tonychenl <119185964@qq.com>
2013-12-12 22:27:38 +0800

    xx

---
+++ gongpingjia/MianViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  gongpingjia
+//
+//  Created by chen liang on 13-12-12.
+//  Copyright (c) 2013年 gongpingjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MianViewController : UITableViewController<UITextFieldDelegate,UIActionSheetDelegate>
+{
+    NSString *milage;
+    UILabel  *dateLabel;
+    UIActionSheet *dateActionSheet;
+    UIDatePicker  *datePicker;
+    NSDate *date;
+}
+@end
---
+++ gongpingjia/MianViewController.m
@@ -0,0 +1,88 @@
+//
+//  ViewController.m
+//  gongpingjia
+//
+//  Created by chen liang on 13-12-12.
+//  Copyright (c) 2013年 gongpingjia. All rights reserved.
+//
+
+#import "MianViewController.h"
+
+@interface MianViewController ()
+-(void)setDateLabel:(NSDate*)date;
+@end
+
+@implementation MianViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    //actionsheet
+    dateActionSheet = [[UIActionSheet alloc] initWithTitle:@"" delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil];
+    UIToolbar *toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    toolBar.barStyle = UIBarStyleBlackOpaque;
+    [toolBar sizeToFit];
+    NSMutableArray *barItems = [[NSMutableArray alloc] init];
+    UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    [barItems addObject:flexSpace];
+    UIBarButtonItem *doneBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(DatePickerDoneClick:)];
+    [barItems addObject:doneBtn];
+    [toolBar setItems:barItems animated:YES];
+    
+    datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0.0, 44.0, 0.0, 0.0)];
+    datePicker.datePickerMode = UIDatePickerModeDate;
+    [dateActionSheet addSubview:toolBar];
+    [dateActionSheet addSubview:datePicker];
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    //init
+    NSIndexPath *path = [NSIndexPath indexPathForRow:0 inSection:0];
+    UITableViewCell  *cell = [self.tableView cellForRowAtIndexPath:path];
+    dateLabel = (UILabel*)[cell viewWithTag:101];
+    [self setDateLabel:[NSDate date]];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    milage = [textField text];
+    NSLog(@"%@",milage);
+    [textField resignFirstResponder];
+    return NO;
+}
+
+-(void)DatePickerDoneClick:(id)sender
+{
+    date = [datePicker date];
+    [self setDateLabel:date];
+    [dateActionSheet dismissWithClickedButtonIndex:0 animated:YES];
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if ([indexPath section]==0 && [indexPath row] == 0) { //选择上牌日期
+        [dateActionSheet showInView:self.view];
+        [dateActionSheet setBounds:CGRectMake(0, 0, 320, 464)];
+        return;
+    }
+}
+
+-(void)setDateLabel:(NSDate *)date
+{
+    NSDateFormatter *f = [[NSDateFormatter alloc] init];
+    [f setDateFormat:@"YYYY年MM月"];
+    dateLabel.text = [f stringFromDate:date];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
