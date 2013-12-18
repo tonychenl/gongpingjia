1e2a446abfe8822a4d24c7521387719516e8aade
tonychenl <119185964@qq.com>
2013-12-18 13:42:58 +0800

    rrr

--- gongpingjia/KbbViewController.m
+++ gongpingjia/KbbViewController.m
@@ -14,14 +14,10 @@
 
 @interface KbbViewController ()
 -(void)calculatePrice;
+-(void)updateView;
 @end
 
 @implementation KbbViewController
-
--(void)calculatePrice
-{
-    
-}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -39,6 +35,7 @@
     _avgMile = 2100;
     _bFirst = TRUE;
     _city = @"南京";
+    _mUserCondition = @"较好";
     
     MianViewController *root = [self.navigationController.viewControllers objectAtIndex:0];
     _brandModel = [root valueForKey:@"brandModel"];
@@ -115,8 +112,8 @@
                 _avgMile = [[tmpdic objectForKey:@"avg_mile"] integerValue];
                 _saleLimit = [[tmpdic objectForKey:@"sale_limit"] integerValue];
                 _tradeInLimit = [[tmpdic objectForKey:@"tradein_limit"] integerValue];
-                _saleRate = [[tmpdic objectForKey:@"sale_rate"] floatValue];
-                _tradeInRate = [[tmpdic objectForKey:@"trade_in_rate"] floatValue];
+                _saleRate = [[tmpdic objectForKey:@"sale_rate"] doubleValue];
+                _tradeInRate = [[tmpdic objectForKey:@"trade_in_rate"] doubleValue];
                 _city = [responseObject objectForKey:@"current_city"];
                 
                 
@@ -128,6 +125,12 @@
                     _mModelMile = [NSString stringWithFormat:@"%dl",_avgMile];
                 }
                 
+                _mUserMile = [_mModelMile integerValue];
+                double d = 100.0 * (_mUserMile/(float)_tradeInLimit);
+                self.mliSlider.value = (int)d;
+                
+                [self calculatePrice];
+                [self updateView];
                 [SVProgressHUD showSuccessWithStatus:[responseObject objectForKey:@"msg"]];
             }else{
                 [SVProgressHUD showErrorWithStatus:[responseObject objectForKey:@"msg"]];
@@ -151,8 +154,77 @@
 }
 
 - (IBAction)mliChange:(id)sender {
+    if (_bFirst) {
+        _bFirst = FALSE;
+        return;
+    }
+    double d = self.mliSlider.value/self.mliSlider.maximumValue;
+    _mUserMile = (int)(d * _tradeInLimit);
+    [self calculatePrice];
+    [self updateView];
 }
 
 - (IBAction)statusChange:(id)sender {
+    UISlider *slider = (UISlider*)sender;
+    double d1 = slider.value;
+    double d2 = slider.maximumValue;
+    double d3 = 100.0f * (d1/d2);
+    if (d3 < 33.0f) {
+        _mUserCondition = @"一般";
+        self.statusSlider.value = 0;
+    }else if (d3 > 66.0f) {
+        _mUserCondition = @"优秀";
+        self.statusSlider.value = (int)d2;
+    }else{
+        _mUserCondition = @"较好";
+        self.statusSlider.value = (int)(d2/2.0f);
+    }
+    [self calculatePrice];
+    [self updateView];
+}
+
+-(void)calculatePrice
+{
+    _mListPrice = (int)(0.9f * _basePrice);
+    double d1 = _mUserMile;
+    double d2 = 0.5f * (1.0f + (d1 - _avgMile)/(float)(_avgMile-_tradeInLimit));
+    if (d2 < 0.0f) {
+        d2 = 0.0f;
+    }
+    _mListPrice = (int)(_mListPrice + d2 * (0.2f * _basePrice));
+    _mListPrice += _optionPrice;
+    if ([@"一般" isEqualToString:_mUserCondition]) {
+        _mListPrice = (int)(0.913f * _mListPrice);
+    }
+    if ([_mUserCondition isEqualToString:@"优秀"]) {
+        _mListPrice = (int)(1.059f * _mListPrice);
+    }
+    
+    double d3 = 0.03f;
+    //if (d1 < _saleLimit) {
+        d3 += 0.05f * (1.0f - d1 / _saleLimit);
+    //}
+    _mPrivatePartyPrice = (int)((float)_mListPrice / (1.0f + d3));
+    
+    double d4 = 0.08f;
+    if (d1 < _tradeInLimit) {
+        d4 += 0.08f * (1.0f - d1 / (float)_tradeInLimit);
+    }
+    _mTradeInPrice = (int)((float)_mListPrice/(1.0f + d4));
+    
+}
+
+-(void)updateView
+{
+    self.price.text = [NSString stringWithFormat:@"￥%d",_mPrivatePartyPrice];
+    self.price1.text = self.price.text;
+    self.mliLabel.text = [NSString stringWithFormat:@"您车的里程数：%d公里",_mUserMile];
+    self.mliage.text = [NSString stringWithFormat:@"%d公里",_mUserMile];
+    self.statusLabel.text = [NSString stringWithFormat:@"您车的车况：%@",_mUserCondition];
+    self.makertMaxPrice.text = [NSString stringWithFormat:@"￥%d",_marketHighPrice];
+    self.makertAvgPrice.text = [NSString stringWithFormat:@"￥%d",_marketPrice];
+    self.makertMinPrice.text = [NSString stringWithFormat:@"￥%d",_marketLowPrice];
+    self.changePrice.text = [NSString stringWithFormat:@"￥%d",_mTradeInPrice];
+    self.suggestPrice.text = [NSString stringWithFormat:@"￥%d",_mListPrice];
 }
 @end
